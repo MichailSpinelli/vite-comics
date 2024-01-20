8155e8a146b26a935a79e03effb704b2762eb4b4
Michail Spinelli <michailspinelli96@gmail.com>
2024-01-20 23:58:20 +0100

    metà

--- src/App.vue
+++ src/App.vue
@@ -22,11 +22,11 @@ import AppFooter from './components/AppFooter.vue';
 <template>
 
   <header>
-    <div>
+    <div class="logo">
       <img src="../img/dc-logo.png" alt="logo">
     </div>
 
-    <div class="header-left">
+    <div class="header-right">
       <button>
         CHARACTER
       </button>
@@ -48,33 +48,51 @@ import AppFooter from './components/AppFooter.vue';
 
     <div class="main-low">
 
-      <div>
-        <a>
+      <div class="main-links">
+        <div class="main-links-img-container">
+          <img src="../img/buy-comics-digital-comics.png" alt="comics">
+        </div>
+        <span>
           DIGITAL COMICS
-        </a>
-        <img src="../img/buy-comics-digital-comics.png" alt="comics">
+        </span>
       </div>
 
-      <div>
-        <a>
-          DIGITAL COMICS
-        </a>
-        <img src="../img/buy-comics-digital-comics.png" alt="comics">
+      <div class="main-links">
+        <div class="main-links-img-container">
+          <img src="../img/buy-comics-merchandise.png" alt="merchandise">
+        </div>
+        <span>
+          DC MERCHANDISE
+        </span>
       </div>
 
-      <div>
-        <a>
-          DIGITAL COMICS
-        </a>
-        <img src="../img/buy-comics-digital-comics.png" alt="comics">
+      <div class="main-links">
+        <div class="main-links-img-container">
+          <img src="../img/buy-comics-subscriptions.png" alt="subscription">
+        </div>
+        <span>
+          SUBSCRIPTION
+        </span>
       </div>
 
-      <div>
-        <a>
-          DIGITAL COMICS
-        </a>
-        <img src="../img/buy-comics-digital-comics.png" alt="comics">
+      <div class="main-links">
+        <div class="main-links-img-container">
+          <img src="../img/buy-comics-shop-locator.png" alt="shop locator">
+        </div>
+        <span>
+          COMIC SHOP LOCATOR
+        </span>
       </div>
+
+      <div class="main-links">
+        <div class="main-links-img-container">
+          <img src="../img/buy-dc-power-visa.svg" alt="visa">
+        </div>
+        <span>
+          DC POWER VISA
+        </span>
+      </div>
+
     </div>
   </main>
 
@@ -82,13 +100,30 @@ import AppFooter from './components/AppFooter.vue';
     <div class="footer-top">
 
       <div class="links">
-        <div>
-        <h3>
-          DC COMICS
-        </h3>
-        <a href="">
-          Character
-        </a>
+
+          <h3>
+            DC COMICS
+          </h3>
+
+          <div class="footer-links">
+
+            <a href="">
+            Character
+            </a>
+            <a href="">
+              Character
+            </a>
+            <a href="">
+              Character
+            </a>
+            <a href="">
+              Character
+            </a>
+            <a href="">
+              Character
+            </a>
+
+          </div>
       </div>
 
       <div>
@@ -127,17 +162,24 @@ import AppFooter from './components/AppFooter.vue';
 header {
   display: flex;
   justify-content: space-around;
+  padding: 30px;
 }
 
-.header-left {
-  padding: 30px;
+.logo{
+  height: 50px;
+  width: 50px;
+  background-color: red;
+}
+
+.logo img {
+  max-width: 100%;
 }
 
 .main-top {
   background-color: #1C1C1C;
   color: white;
-  padding: 50px;
-  font-size: 25px;
+  padding: 30px;
+  font-size: 15px;
 }
 
 .main-low {
@@ -148,10 +190,31 @@ header {
   justify-content: space-around;
 }
 
+.main-low > * {
+  font-size: 0.8em;
+}
+
+.main-links {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.main-links-img-container{
+    height: 40px;
+    width: 40px;
+    margin-right: 10px;
+}
+
+.main-links-img-container img{
+    width: 100%;
+}
+
 .footer-top {
   background-image: url(../img/footer-bg.jpg);
   height: 100px;
 }
+
 
 .links {
   display: flex;
